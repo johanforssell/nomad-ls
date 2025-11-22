190c67400b6baa35055c0cddf51af30c9b207c77
loczek <30776250+loczek@users.noreply.github.com>
2025-11-22 09:25:22 +0100

    remove temporary test

--- internal/lsp/lsp_test.go
+++ internal/lsp/lsp_test.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclparse"
-	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/loczek/nomad-ls/internal/schema"
 	"go.lsp.dev/protocol"
 )
@@ -28,34 +27,6 @@ func TestByteCount(t *testing.T) {
 	if actuallCount != int(predictedCount) {
 		t.Errorf("expected: %d, recieved: %d", actuallCount, predictedCount)
 	}
-}
-
-func TestSimpleParse(t *testing.T) {
-	hclFile := LoadSampleFile(LOKI_NOMAD_FILE_PATH)
-
-	bc, _ := hclFile.Body.Content(schema.SchemaMapBetter["root"])
-	// t.Logf("%+v", bc.Blocks.ByType())
-	// t.Logf("ed: %+v", bc)
-
-	x := hclFile.Body.(*hclsyntax.Body)
-	// t.Logf("body syntax cast: %#v", x.SrcRange.)
-
-	t.Logf("body range: %d:%d to %d:%d", x.SrcRange.Start.Line, x.SrcRange.Start.Column, x.SrcRange.End.Line, x.SrcRange.End.Column)
-
-	block := bc.Blocks[2]
-	bc, _ = block.Body.Content(schema.SchemaMapBetter["job"])
-	// t.Logf("%+v", bc.Blocks.ByType())
-	// t.Logf("ed: %#v", bc)
-	t.Logf("job block: %#v", block.DefRange)
-
-	block = bc.Blocks[0]
-	bc, _ = block.Body.Content(schema.SchemaMapBetter["group"])
-	// t.Logf("%+v", bc.Blocks.ByType())
-	// t.Logf("ed: %#v", bc)
-	t.Logf("group block: %#v", block.DefRange)
-
-	// t.Logf("%+v", attr)
-	// t.Logf("blocks: %v", blocks)
 }
 
 func TestServiceBlockHoverInformation(t *testing.T) {
